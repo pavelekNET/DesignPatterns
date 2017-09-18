1bb52a478d8821f79454b875abd39ed37b7dbda3
Marek Pavelek <pavelekmarek@gmail.com>
2017-09-19 00:45:06 +0200

    Composite pattern

---
+++ StructuralDesignPatterns/StructuralDesignPatterns/Composition/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Structural.Composition.Loggers;
+
+namespace Structural.Composition
+{
+    public class Client
+    {
+        public static void Main()
+        {
+            var fileLogger = new FileLogger("c:\\path");
+            var httpLogger = new HttpLogger(new Uri("www.idnes.cz/logs"));
+            var outputLogger = new OutputLogger();
+
+            var multiLogger = new CompositeLogger(fileLogger, httpLogger, outputLogger);
+            multiLogger.Log("LOG FOR MULTIPLE LOGGERS");
+        }
+    }
+}
---
+++ StructuralDesignPatterns/StructuralDesignPatterns/Composition/Loggers/CompositeLogger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Structural.Decorator.Component;
+
+namespace Structural.Composition.Loggers
+{
+    public class CompositeLogger : ILogger
+    {
+        private readonly List<ILogger> _loggers;
+
+        public CompositeLogger()
+        {
+            _loggers = new List<ILogger>();
+        }
+
+        public CompositeLogger(params ILogger[] loggers) : this()
+        {
+            _loggers.AddRange(loggers);
+        }
+
+        public void Log(string message)
+        {
+            foreach (var logger in _loggers)
+            {
+                logger.Log(message);
+            }  
+        }
+
+        public void Add(ILogger logger)
+        {
+            _loggers.Add(logger);
+        }
+    }
+}
