b49cd50cf23153f21765e006d2621de5780daeb0
Marek Pavelek <pavelekmarek@gmail.com>
2017-10-05 01:06:38 +0200

    maybe

--- StructuralDesignPatterns/Common/Repository/IRepository.cs
+++ StructuralDesignPatterns/Common/Repository/IRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Common.Repository
+﻿namespace Common.Repository
 {
     public interface IRepository<TModel> : IReadonlyRepository<TModel>
     {
